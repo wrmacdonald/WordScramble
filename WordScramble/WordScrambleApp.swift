c389e1a37183272bf57f1cced9a407c4f6f78749
Wes MacDonald <wrmacdonald2016@gmail.com>
2024-03-08 09:51:24 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Wes MacDonald on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
